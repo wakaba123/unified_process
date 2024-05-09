8fcc1160c7c1084226b4edbe9392a4eb4f5c4cc9
blues <blues243134@gmail.com>
2024-05-09 21:25:49 +0800

    modify

--- power_model/freq_count.py
+++ power_model/freq_count.py
@@ -24,8 +24,8 @@ f = open('result.csv','w')
 f.write('cpu,freq,total_time,state0,state1,state2,state3\n')
 
 for i in range(8):
-    cpu_freq_data_temp = cpu_freq_data[cpu_freq_data['cpu'] == i]
-    cpu_idle_data_temp = cpu_idle_data[cpu_idle_data['cpu'] == i]
+    cpu_freq_data_temp = cpu_freq_data[cpu_freq_data['cpu'] == i].sort_values(by='start')
+    cpu_idle_data_temp = cpu_idle_data[cpu_idle_data['cpu'] == i].sort_values(by='start')
 
     if i in policy[0]:
         frequencies = freq_lists[0]
@@ -36,7 +36,7 @@ for i in range(8):
 
     print(frequencies)
     for frequency in frequencies:
-        filtered_cpu_freq_data = cpu_freq_data_temp[cpu_freq_data['value'] == frequency]
+        filtered_cpu_freq_data = cpu_freq_data_temp[cpu_freq_data_temp['value'] == frequency]
         if filtered_cpu_freq_data.empty:
             total_time_frequency = 0
         else:
@@ -48,19 +48,29 @@ for i in range(8):
     results = {}
 
     for frequency in frequencies:
-        filtered_cpu_freq_data = cpu_freq_data_temp[cpu_freq_data['value'] == frequency]
+        filtered_cpu_freq_data = cpu_freq_data_temp[cpu_freq_data_temp['value'] == frequency]
         cpu_state_occupancy = {cpu_state: 0 for cpu_state in range(4)}
 
         for index, row in filtered_cpu_freq_data.iterrows():
             freq_start = row['start']
             freq_end = row['end']
 
-            for _,temp in cpu_idle_data.iterrows():
-                state_start = temp['start']
-                state_end = temp['end'] 
-                state = temp['value']
+            state_temp = 0
+            while True:
+                if state_temp >= len(cpu_idle_data_temp):
+                    break
+                
+                state_start = cpu_idle_data.iloc[state_temp]['start']
+                state_end = cpu_idle_data.iloc[state_temp]['end']
+                state = cpu_idle_data.iloc[state_temp]['value']
 
-                # state 完全在 freq里面
+                if state_start > freq_end: # 已经离开该区间
+                    break
+
+                if state_end < freq_start: # 还没到该区间
+                    state_temp += 1
+                    continue
+
                 if state_start >= freq_start and state_end <= freq_end :
                     cpu_state_occupancy[state] += state_end - state_start
 
@@ -75,6 +85,8 @@ for i in range(8):
                 #state 右边一部分在外面
                 if state_start >= freq_start and state_start < freq_end and state_end > freq_end:
                     cpu_state_occupancy[state] += (freq_end - state_start)
+
+                state_temp += 1
 
         results[frequency] = cpu_state_occupancy
         f.write(f'{i},{frequency},{freq_time_dict[frequency]},{cpu_state_occupancy[0]},{cpu_state_occupancy[1]},{cpu_state_occupancy[2]},{cpu_state_occupancy[3]}\n')
